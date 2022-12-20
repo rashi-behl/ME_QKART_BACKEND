ccc05c041e0446a0194dbded0d80b65de29abd75
rashi-behl <rashi.behl97@gmail.com>
2022-12-20 22:55:53 +0530

    MongodbURL

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,3 +1,3 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri <add-url-connection-string-here> --drop --collection users --file data/export_qkart_users.json
-mongoimport --uri <add-url-connection-string-here> --drop --collection products --file data/export_qkart_products.json
+mongoimport --uri "mongodb+srv://mongodbUser:mongodbUserPassword@qkart-node.ut4qzjf.mongodb.net/test" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://mongodbUser:mongodbUserPassword@qkart-node.ut4qzjf.mongodb.net/test" --drop --collection products --file data/export_qkart_products.json
